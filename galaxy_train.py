1f4195bf6e183586bcd4e7ce6de30395683a4248
pedrobeirao <pbeirao7@gmail.com>
2019-01-23 22:18:00 +0100

    Add files via upload

---
+++ galaxy_train.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 22 21:38:52 2019
+
+@author: pedrobeirao
+"""
+
+from keras.applications.resnet50 import ResNet50, preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Activation, Flatten, Dropout
+from keras.models import Sequential, Model
+from keras.optimizers import SGD, Adam
+from keras.callbacks import ModelCheckpoint
+import matplotlib.pyplot as plt
+
+HEIGHT = 300
+WIDTH = 300
+
+base_model = ResNet50(weights='imagenet', 
+                      include_top=False, 
+input_shape=(HEIGHT, WIDTH, 3))
+
+
+TRAIN_DIR = "~/galaxy_dataset/train"
+HEIGHT = 300
+WIDTH = 300
+BATCH_SIZE = 8
+
+train_datagen =  ImageDataGenerator(
+      preprocessing_function=preprocess_input,
+      rotation_range=90,
+      horizontal_flip=True,
+      vertical_flip=True
+    )
+
+train_generator = train_datagen.flow_from_directory(TRAIN_DIR, 
+                                                    target_size=(HEIGHT, WIDTH), batch_size=BATCH_SIZE)
+
+
+def build_finetune_model(base_model, dropout, fc_layers, num_classes):
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    x = base_model.output
+    x = Flatten()(x)
+    for fc in fc_layers:
+        # New FC layer, random init
+        x = Dense(fc, activation='relu')(x) 
+        x = Dropout(dropout)(x)
+
+    # New softmax layer
+    predictions = Dense(num_classes, activation='softmax')(x) 
+    
+    finetune_model = Model(inputs=base_model.input, outputs=predictions)
+
+    return finetune_model
+
+class_list = ["E", "S0"]
+FC_LAYERS = [1024, 1024]
+dropout = 0.5
+
+finetune_model = build_finetune_model(base_model, 
+                                      dropout=dropout, 
+                                      fc_layers=FC_LAYERS, 
+                                      num_classes=len(class_list))
+
+NUM_EPOCHS = 20
+#BATCH_SIZE = 8
+num_train_images = 28
+
+adam = Adam(lr=0.00001)
+finetune_model.compile(adam, loss='categorical_crossentropy', metrics=['accuracy'])
+
+filepath="ResNet50" + "_model_weights.h5"
+checkpoint = ModelCheckpoint(filepath, monitor=["acc"], verbose=1, mode='max')
+callbacks_list = [checkpoint]
+
+history = finetune_model.fit_generator(train_generator, epochs=NUM_EPOCHS, workers=8, 
+                                       steps_per_epoch=num_train_images // BATCH_SIZE, 
+                                       shuffle=True, callbacks=callbacks_list)
+
+
+
+# Plot the training and validation loss + accuracy
+def plot_training(history):
+    acc = history.history['acc']
+#    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+#    val_loss = history.history['loss']
+    epochs = range(len(acc))
+
+    plt.plot(epochs, acc, 'r.')
+    plt.plot(epochs, loss, 'r')
+    plt.title('Training and validation accuracy')
+
+    # plt.figure()
+    # plt.plot(epochs, loss, 'r.')
+    # plt.plot(epochs, val_loss, 'r-')
+    # plt.title('Training and validation loss')
+    plt.show()
+
+plot_training(history)
+
+#plt.savefig('acc_vs_epochs.png')
